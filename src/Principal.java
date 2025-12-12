1ada36133e1482e159e3e0faab04e348f46f6764
Mariana <marianaelimasilva@gmail.com>
2025-12-12 20:31:02 -0300

    Primeira versão!

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Principal {
+
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme(); //esquerda guarda e direita cria
+
+        meuFilme.nome = "O poderoso chefao";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.incluidoNoPlano = true;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(9);
+
+        System.out.println("Média de avaliações: "+ meuFilme.pegaMedia());
+
+    }
+
 }
