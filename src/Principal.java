d135ee4f29bc1a5378dbce3d92651500660b40cf
Mariana <marianaelimasilva@gmail.com>
2025-12-15 23:05:14 -0300

    Versão atualizada do Screen Match com os conceitos de Getters e Setters, Override e extends.

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,58 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
 
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme(); //esquerda guarda e direita cria
 
-        meuFilme.nome = "O poderoso chefao";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
-        meuFilme.incluidoNoPlano = true;
+        meuFilme.setNome("The Matrix");
+        meuFilme.setAnoDeLancamento(1999);
+        meuFilme.setDuracaoEmMinutos(135);
+        meuFilme.setIncluidoNoPlano(true);
+        System.out.println("Duração do filme: "+ meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.getNome();
+        meuFilme.getAnoDeLancamento();
+        meuFilme.getDuracaoEmMinutos();
+        meuFilme.getisIncluidoNoPlano();
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(9);
 
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média de avaliações: "+ meuFilme.pegaMedia());
 
-    }
+        Serie serie = new Serie();
+        serie.setNome("La Casa de Papel");
+        serie.setAnoDeLancamento(2017);
+        serie.setTemporadas(5);
+        serie.setIncluidoNoPlano(true);
+        serie.setAtiva(true);
+        serie.setMinutosPorEpisodio(45);
+        serie.setEpisodiosPorTemporada(10);
+        serie.exibeFichaTecnica();
 
+        System.out.println("Duração da série: " + serie.getDuracaoEmMinutos() + " minutos.");
+
+        Filme outroFilme = new Filme();
+
+        outroFilme.setNome("John Wick");
+        outroFilme.setAnoDeLancamento(2014);
+        outroFilme.setDuracaoEmMinutos(101);
+        outroFilme.setIncluidoNoPlano(true);
+        outroFilme.exibeFichaTecnica();
+
+        //calculadora juntando filmes e series
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(serie);
+        System.out.println("Tempo total: "+ calculadora.getTempoTotal());
+
+    }
 }
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,70 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public void exibeFichaTecnica() {
+
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+        System.out.println("Incluído no plano: " + incluidoNoPlano);
+
+    }
+
+    public void avalia(double nota) { //apenas dá, sem receber
+
+        somaAvaliacoes += nota;
+        totalDeAvaliacoes++;
+
+    }
+
+    public double pegaMedia() { //recebe
+
+        return somaAvaliacoes / totalDeAvaliacoes;
+
+    }
+
+    public int getTotalDeAvaliacoes() { //metodo acessor/ getter
+        return totalDeAvaliacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean getisIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNome(String nome) { //setter
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+}
